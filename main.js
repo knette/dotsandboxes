9de8c3a283b816e2ee2afd8dd6c731e1f7a8b30b
kristina nette <kristina.nette@gmail.com>
2018-02-05 15:44:05 -0800

    working still

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 var $line =  $('.line')
 var $vertLine = $('.vert-line')
 var $box1 = $('.box1') 
+var $score = $('#score-player1').text()
+var currentPlayer = []
+//update above to players score instead of player 1.
 
 //when a line is clicked its color changes
+//if all 4 sides of box1 are not the color they started as
+//update inside box color to players color
 function clickHandler() {
     $(this).css("background-color","red")
-    if ($box1.css('background-color') != "rgb(255, 221, 206)") {
+    if ($box1.css('background-color') != 'rgb(255, 221, 206)') {
         $('.hotbox1').css('background-color', 'blue')
-        //update above to players color. 
-        //score +1 
+        $score = parseInt($score) + 1
+    }
 }
 $line.on('click', clickHandler)
 $vertLine.on('click', clickHandler)
 
-//if all 4 sides of box1 are not the color they started as
-//update inside box color to players color
-
-
-}
