dd1d20e9096b9ceeac8b525d27b37767c2f76ee1
kristina nette <kristina.nette@gmail.com>
2018-02-05 14:53:10 -0800

    added box1 tagging and jquery

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
 var $line =  $('.line')
 var $vertLine = $('.vert-line')
+var $box1 = $('.box1') 
 
 //when a line is clicked its color changes
 function clickHandler() {
-    $(this).css("background-color","yellow")
+    $(this).css("background-color","red")
+    if ($box1.css('background-color') != "rgb(255, 221, 206)") {
+        $('.hotbox1').css('background-color', 'blue')
+        //update above to players color. 
+        //score +1 
 }
 $line.on('click', clickHandler)
 $vertLine.on('click', clickHandler)
 
-console.log("hello js works")
+//if all 4 sides of box1 are not the color they started as
+//update inside box color to players color
+
+
+}
