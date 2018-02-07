8930482daa6fd4bb8f6b53bb3a0babce47038290
kristina nette <kristina.nette@gmail.com>
2018-02-06 16:11:59 -0800

    big update, js switching and scoring logic, css dry code, readme update

--- main.js
+++ main.js
@@ -9,14 +9,11 @@ var playersTurn = $('#players-turn')
 var game = {
     players: [{name: 'Player 1'}, {name: 'Player 2'}],
     currentPlayer: null,
-    winner: false,
     init: function() {
-        console.log('game begins')
         game.currentPlayer = game.players[0]
     },
     switchPlayer: function() {
         if (game.currentPlayer === game.players[0]) {
-            //if score increases player stays the same
             game.currentPlayer = game.players[1]
             playersTurn.text(game.currentPlayer.name)
         } else {
@@ -26,16 +23,29 @@ var game = {
     }
 }
 //On each click of a line, change the color and the player
-//unless the player wins the box. If the player wins the
-//box update the inside of the box to their color. 
+    //unless the player wins the box. If the player wins the
+    //box update the inside of the box to their color. 
 //++update inside box color to players color
 function clickHandler() {
     $(this).addClass('clicked')
-    for (i = 1; i < 26; i++) {
-        if ($('.box' + i + '.clicked').length === 4){
-            $('.hotbox' + i).addClass('player1won')
-        } else (game.switchPlayer())
+    console.log(game.currentPlayer.name)
+    for (i = 1; i < 25; i++) {
+        if (($('.box' + i + '.clicked').length === 4) && (game.currentPlayer.name === 'Player 1')){
+            if(!$('.hotbox' + i).hasClass('player2won') && !$('.hotbox' + i).hasClass('player1won')) {
+              $('.hotbox' + i).addClass('player1won') 
+              game.switchPlayer()
+            }
+            
+            //if hotboxi doesnt have a class player2won add class of player1won
+        } else if (($('.box' + i + '.clicked').length === 4) && (game.currentPlayer.name === 'Player 2')){
+            if(!$('.hotbox' + i).hasClass('player1won') && !$('.hotbox' + i).hasClass('player2won')) {
+                $('.hotbox' + i).addClass('player2won') 
+                game.switchPlayer() 
+              }
+        }     
     }
+    game.switchPlayer()
+    
     $scorePlayer1.text($('.box.player1won').length)
     $scorePlayer2.text($('.box.player2won').length)
 }
